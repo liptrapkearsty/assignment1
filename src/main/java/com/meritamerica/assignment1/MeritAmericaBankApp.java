9e0725a858c9c6f6f63e625cb43b53bb5e26655c
Kearsty <liptrapkearsty@gmail.com.>
2020-02-28 20:13:18 -0600

    test try

--- src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment1/MeritAmericaBankApp.java
@@ -1,5 +1,11 @@
 package com.meritamerica.assignment1;
-
-public class MeritAmericaBankApp {
-	
-}
+ 
+    public class MeritAmericaBankApp {
+            public static void main (String[]args) {
+                AccountHolder customer1 = new AccountHolder();
+                customer1.setFirstName("Roy");
+                customer1.setMiddleName("Johnson");
+                customer1.setLastName("Pemberton");
+                customer1.setSSN("");
+            }
+ 
--- src/main/java/com/meritamerica/assignment1/SavingsAccount.java
+++ src/main/java/com/meritamerica/assignment1/SavingsAccount.java
@@ -1,5 +1,32 @@
 package com.meritamerica.assignment1;
-
-public class SavingsAccount {
-	
+ 
+public class SavingsAccount extends MeritAmericaBankApp{
+    
+    private double balance;
+    
+    public SavingsAccount() {
+        balance = 0.0;
+    }
+    
+    SavingsAccount(double openingBalance) {
+    }
+    double getBalance() {
+        return balance;
+    }
+    double getInterestRate() {
+        return 0;
+    }
+    public void withdraw(double amount) {
+        if (amount <= balance)
+            balance = balance - amount;
+    }
+    public void deposit(double amount) {
+        balance = balance + amount;
+    }
+    double futureValue(int years) {
+        return 0;
+    }
+    String toString()
+}
+    
 }
